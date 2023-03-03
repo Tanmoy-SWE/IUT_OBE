3c3ac3e83f6c18dfed64db6d643f91427fadce7d
Lamia45711704 <ahsanhabib3@iut-dhaka.edu>
2023-03-03 21:33:32 +0600

    Admin access of DB

--- IUT_OBE/authentication/admin.py
+++ IUT_OBE/authentication/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Course, AssignedCourses, CO
+
 # Register your models here.
+
+admin.site.register(AssignedCourses)
+admin.site.register(CO)
+admin.site.register(Course)
